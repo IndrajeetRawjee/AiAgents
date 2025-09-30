1da9c678cc1796a45c950302e59a52e051809526
Indrajeet Rawjee <indrajeetrawjee4@gmail.com>
2025-09-30 17:17:15 +0000

    Done till Inputs. Next is Messages

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,8 +8,12 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    if len(sys.argv) < 2:
+        print("I need a prompt!")
+        sys.exit(1)
+    prompt =  sys.argv[1]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
     )
     print(response.text)
     if response is None or response.usage_metadata is None:
